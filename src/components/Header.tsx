038240c96b70cd3a1ae77a2a65c33bccd0b10fd1
iych0 <jpksfredfrtupk@gmail.com>
2025-11-06 21:09:26 +0500

    feat: qs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,21 +5,12 @@ import {useGameStore} from "../store/gameStore.ts";
 import {useShallow} from "zustand/react/shallow";
 
 const Header = () => {
-    const { players, restartGame } = useGameStore(
-        useShallow((state) => ({
-            players: state.players,
-            restartGame: state.restartGame,
-        }))
-    );
+    const restartGame = useGameStore(state => state.restartGame);
 
     return (
         <div className='flex flex-row justify-between w-full px-6 items-center'>
             <div className="h-12 w-12"></div>
-            <div className="flex gap-4 text-5xl">
-                <span className={`text-ctp-${players[0].color}`}>{players[0].winsCount}</span>
-                <span>:</span>
-                <span className={`text-ctp-${players[1].color}`}>{players[1].winsCount}</span>
-            </div>
+            <Scoreboard />
             <div className={`flex relative h-12 w-12 justify-center items-center rounded-full transition duration-200
             hover:bg-ctp-surface1 after:content-['Начать_сначала?'] after:opacity-0 hover:after:opacity-100
             after:transition-opacity after:duration-100
@@ -30,5 +21,21 @@ const Header = () => {
         </div>
     );
 };
+
+const Scoreboard = () => {
+    const { players } = useGameStore(
+        useShallow((state) => ({
+            players: state.players,
+        }))
+    );
+
+    return (
+        <div className="flex gap-4 text-5xl">
+            <span className={`text-ctp-${players[0].color}`}>{players[0].winsCount}</span>
+            <span>:</span>
+            <span className={`text-ctp-${players[1].color}`}>{players[1].winsCount}</span>
+        </div>
+    )
+}
 
 export default Header;
